32bbf22f4c4c99bfb603173028e67861d8a273d8
Bali Merta <lovacode@Balis-MacBook-Pro.local>
2020-10-25 17:05:40 +0800

    fix route

--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('webhook', 'API\WebhookController@handle');
+Route::post('webhook', 'API\WebhookController@handle');
