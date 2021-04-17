0862b1f2c6b91d8b3b7d12e25cff434e2645e92f
bali_appkey <bali.appkey@gmail.com>
2021-04-17 22:24:21 +0800

    Update

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -9,7 +9,7 @@
   <meta name="description" content="">
   <meta name="author" content="">
 
-  <title>Toyota Home - Dashboard</title>
+  <title>Tempo Dulu Kopi</title>
 
   <!-- Custom fonts for this template-->
   <link href="{{asset('admin/vendor/fontawesome-free/css/all.min.css')}}" rel="stylesheet" type="text/css">
